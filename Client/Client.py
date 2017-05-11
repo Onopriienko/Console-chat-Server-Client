e157abbd245c491a5165e99aa1c4ffc3c65b458c
Andrei <coolcrull@gmail.com>
2017-05-12 00:47:50 +0300

    Com

--- Client/Client.py
+++ Client/Client.py
@@ -1,10 +1,11 @@
 import socket
 
-sock = socket.socket()
-sock.connect(('192.168.0.105', 9090))
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(('127.0.0.1', 9090))
 sock.send(str.encode('hi'))
 
 data = sock.recv(1024)
+data = bytes.decode(data)
 sock.close()
 
-print (data)
+print(data)
--- Server/Server.py
+++ Server/Server.py
@@ -1,16 +1,16 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM )
-sock.bind(('192.168.0.105', 9090))
-sock.listen(1)
+sock.bind(('127.0.0.1', 9090))
+sock.listen(5)
 conn, addr = sock.accept()
 
-print ('connected:', addr)
+print('connected:', addr)
 
 while True:
     data = conn.recv(1024)
+    print(data)
     if not data:
         break
     conn.send(data)
-
 conn.close()
