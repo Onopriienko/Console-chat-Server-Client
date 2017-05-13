2717f8346a3afe2238240f99743a8e940c6d991f
Andrei <coolcrull@gmail.com>
2017-05-13 12:35:37 +0300

    Commit

--- Server/Server.py
+++ Server/Server.py
@@ -1,11 +1,8 @@
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM )
-sock.bind(('127.0.0.1', 9090))
-sock.listen(5)
-conn, addr = sock.accept()
-
-print('connected:', addr)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind(('192.168.0.105', 5050))
+print('Server > 192.168.0.105 : 5050')
 
 while True:
     data = conn.recv(1024)
