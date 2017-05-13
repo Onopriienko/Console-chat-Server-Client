b1be0f4877943320ecf36dabf686ff8a74efc744
Andrei <coolcrull@gmail.com>
2017-05-13 15:41:28 +0300

    Commit

--- Client/Client.py
+++ Client/Client.py
@@ -1,7 +1,7 @@
 import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client_socket.connect(('192.168.0.105', '5050'))
+client_socket.connect(('192.168.0.105', 5050))
 connection_msg = '< ' + 'Ubuntu' + ' connected >'
 client_socket.sendall(str.encode(connection_msg))
 
