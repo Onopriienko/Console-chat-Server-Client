02e0b076d43e8e80a92f5187725d8a538f1585be
Andrei <coolcrull@gmail.com>
2017-05-13 16:57:48 +0300

    Commit

--- Client/Client.py
+++ Client/Client.py
@@ -1,12 +1,22 @@
 import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client_socket.connect(('192.168.0.105', 5050))
+client_socket.connect(('192.168.0.105', 55555))
 connection_msg = '< ' + 'Ubuntu' + ' connected >'
-client_socket.sendall(str.encode(connection_msg))
+#client_socket.sendall(str.encode(connection_msg))
 
-data = client_socket.recv(1024)
-data = bytes.decode(data)
-client_socket.close()
+while True:
+    msg = input('Ch: ')
+    client_socket.sendall(str.encode(msg))
+    data = client_socket.recv(1024)
 
-print(data)
+    # обработка информации
+    print(data.decode('utf-8'))
+
+    if not msg:
+        break
+
+
+
+
+
--- Server/Server.py
+++ Server/Server.py
@@ -1,8 +1,8 @@
 import socket
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind(('192.168.0.105', 5050))
-print('Server > 192.168.0.105 : 5050')
+server_socket.bind(('192.168.0.105', 55555))
+#print('Server > 192.168.0.105 : 5050')
 
 clients = []
 
