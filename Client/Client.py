e9e51d83a3c8021ea4ab4c4fd85c050695566cfe
Andrei <coolcrull@gmail.com>
2017-05-16 00:15:34 +0300

    Commit

--- Client/Client.py
+++ Client/Client.py
@@ -1,6 +1,7 @@
 import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 client_socket.connect(('127.0.0.1', 55555))
 name = input('Enter your name: ')
 connection_msg = '< ' + name + ' connected >'
@@ -9,15 +10,17 @@ client_socket.sendall(str.encode(connection_msg))
 
 
 while True:
+    data = client_socket.recv(1024)
+    print(data.decode('utf-8'))
     msg = input(name + ': ')
     client_socket.sendall(str.encode(name) + b': ' + str.encode(msg))
-    data = client_socket.recv(1024)
 
     # обработка информации
-    print(data.decode('utf-8'))
+    # print(data.decode('utf-8'))
 
     if not msg:
         break
+
 
 client_socket.close()
 
--- Server/Server.py
+++ Server/Server.py
@@ -1,6 +1,8 @@
 import socket
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 server_socket.bind(('0.0.0.0', 55555))
 print('< Server port > 55555')
 
@@ -15,11 +17,8 @@ while True:
     # обработка информации
     print(data.decode('utf-8'))
 
-
     for client in clients:
-         # if addr != client:
+        # if addr != client:
         server_socket.sendto(data, client)
-
-
 
 server_socket.close()
