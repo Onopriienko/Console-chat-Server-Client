d45daf24aed09195c7e8ad6f8958371a31478fe9
Andrei <coolcrull@gmail.com>
2017-05-15 23:03:59 +0300

    Commit

--- Client/Client.py
+++ Client/Client.py
@@ -1,8 +1,7 @@
 import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-client_socket.connect(('192.168.0.105', 55555))
+client_socket.connect(('127.0.0.1', 55555))
 name = input('Enter your name: ')
 connection_msg = '< ' + name + ' connected >'
 client_socket.sendall(str.encode(connection_msg))
@@ -20,6 +19,7 @@ while True:
     if not msg:
         break
 
+client_socket.close()
 
 
 
--- Server/Server.py
+++ Server/Server.py
@@ -1,8 +1,6 @@
 import socket
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 server_socket.bind(('0.0.0.0', 55555))
 print('< Server port > 55555')
 
