2f6ff0ff4978bad16201fd1a5b1226baa760d3bf
Andrei <coolcrull@gmail.com>
2017-05-25 00:19:18 +0300

    Commit

--- Client/Client.py
+++ Client/Client.py
@@ -1,17 +1,15 @@
 import socket
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-client_socket.connect(('127.0.0.1', 55555))
+client_socket.connect(('127.0.0.1', 55566))
 name = input('Enter your name: ')
-connection_msg = '< ' + name + ' connected >'
-client_socket.sendall(str.encode(connection_msg))
+#connection_msg = '< ' + name + ' connected >'
+#client_socket.sendall(str.encode(connection_msg))
 
 
 
 while True:
-    data = client_socket.recv(1024)
-    print(data.decode('utf-8'))
     msg = input(name + ': ')
     client_socket.sendall(str.encode(name) + b': ' + str.encode(msg))
 
@@ -20,6 +18,8 @@ while True:
 
     if not msg or msg == 'bye':
         break
+    data = client_socket.recv(1024)
+    print(data.decode('utf-8'))
 
 
 client_socket.close()
--- Server/Server.py
+++ Server/Server.py
@@ -1,32 +1,58 @@
 import socket
 from threading import Thread
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-server_socket.bind(('0.0.0.0', 55555))
+server_socket.bind(('0.0.0.0', 55566))
+server_socket.listen(10)
 print('< Server port > 55555')
 
 clients = []
+#recvData = []
 
 
 class TalkToClient(Thread):
-    def __init__(self):
+    def __init__(self, client_sock, client_addr):
         Thread.__init__(self)
+        self.client_sock = client_sock
+        self.client_addr = client_addr
+        print(self.client_addr)
+
+    def run(self):
+        while True:
+            rec_data = self.client_sock.recv(1024)
+            for n in clients:
+                #print(n)
+                if n != self.client_addr:
+                    print(n)
+                    print(self.client_addr)
+                    self.client_sock.sendto(rec_data, n)
+            '''
+            if not rec_data:
+                self.client_sock.send(b'bye')
+                break
+            print(rec_data)
+            for client in clients:
+                self.client_sock.sendto(rec_data, client)
+            '''
+
 
 
 while True:
-    data, addr = server_socket.recvfrom(1024)
+    sock, addr = server_socket.accept()
 
     if addr not in clients:
         clients.append(addr)
+    #print(sock)
+    #print(addr)
+    #print(clients)
 
-    # обработка информации
-    print(data.decode('utf-8'))
-
-    for client in clients:
-        TalkToClient().start()
-        if addr != client:
-            server_socket.sendto(data, client)
+    #for client in clients:
+        #print(client, b'<')
+    TalkToClient(sock, addr).start()
+        #sock.sendto(rec_data, clients)
+        #if addr != client:
+        #server_socket.sendto(data, client)
 
 server_socket.close()
