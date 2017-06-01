e12f28096f7b70bfb13635fe383d75e4578a42b3
Andrei <coolcrull@gmail.com>
2017-06-01 14:38:14 +0300

    Commit

--- Client/Client.py
+++ Client/Client.py
@@ -2,14 +2,31 @@ import socket
 import sys
 import select
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-client_socket.connect(('127.0.0.1', 5555))
-name = input('Enter your name: ')
-#client_socket.settimeout(2)
+host = '127.0.0.1'
+port = 5555
 
-print('Connected to remote host. You can start sending messages')
-print(name + ': ')
+
+def close_chat():
+    print('\nDisconnected from chat server')
+    client_socket.close()
+    sys.exit()
+
+try:
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+except socket.error:
+    print('Failed to create socket.')
+    close_chat()
+
+try:
+    client_socket.connect((host, port))
+except socket.error:
+    print('Connection to server failed')
+    close_chat()
+
+name = input('Enter your name: ')
+print('Connected to remote host. You can start sending messages', '\n' + name + ': ')
+
 
 while True:
     socket_list = [sys.stdin, client_socket]
@@ -19,12 +36,18 @@ while True:
         if sock == client_socket:
             data = sock.recv(4096)
             if not data:
-                print('\nDisconnected from chat server')
-                sys.exit()
+                close_chat()
             else:
                 print(data.decode('utf-8'))
                 print(name, ': ')
         else:
             msg = sys.stdin.readline()
-            client_socket.send(name.encode('utf-8') + b': ' + msg.encode('utf-8'))
-            print(name, ': ')
+            if msg.strip('\n') in ['q', 'quit', 'exit']:
+                close_chat()
+            else:
+                try:
+                    client_socket.send(name.encode('utf-8') + b': ' + msg.encode('utf-8'))
+                    print(name, ': ')
+                except socket.error:
+                    print('Send failed')
+                    close_chat()
